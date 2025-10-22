e0de2ba8a765755cb9ad847140119415d6cdb637
leeao <34832462@qq.com>
2025-10-22 16:40:02 +0800

    Update music_like.js

--- js/music_like.js
+++ js/music_like.js
@@ -2,32 +2,32 @@ var music = [
     {
         name: 'Somebody That I Used To Know',
         author: 'Gotye&Kimbra',
-        url: 'https://leeao8.gitee.io/life/music_like/Somebody That I Used To Know.mp3'
+        url: 'https://life.leeao8.cn/music_like/Somebody That I Used To Know.mp3'
     },
     {
         name: 'Snowdreams',
         author: '班得瑞',
-        url: 'https://leeao8.gitee.io/life/music_like/Bandari-Snowdreams.mp3'
+        url: 'https://life.leeao8.cn/music_like/Bandari-Snowdreams.mp3'
     },
     {
         name: '安妮的仙境',
         author: '班得瑞',
-        url: 'https://leeao8.gitee.io/life/music_like/Bandari-安妮的仙境.mp3'
+        url: 'https://life.leeao8.cn/music_like/Bandari-安妮的仙境.mp3'
     }, {
         name: '初雪',
         author: '班得瑞',
-        url: 'https://leeao8.gitee.io/life/music_like/Bandari-初雪.mp3'
+        url: 'https://life.leeao8.cn/music_like/Bandari-初雪.mp3'
     }, {
         name: '卡布里的月光',
         author: '班得瑞',
-        url: 'https://leeao8.gitee.io/life/music_like/Bandari-卡布里的月光.mp3'
+        url: 'https://life.leeao8.cn/music_like/Bandari-卡布里的月光.mp3'
     }, {
         name: '童年记忆',
         author: '班得瑞',
-        url: 'https://leeao8.gitee.io/life/music_like/Bandari-童年记忆.mp3'
+        url: 'https://life.leeao8.cn/music_like/Bandari-童年记忆.mp3'
     }, {
         name: '故乡的原风景',
         author: '宗次郎',
-        url: 'https://leeao8.gitee.io/life/music_like/宗次郎-故乡的原风景.mp3'
+        url: 'https://life.leeao8.cn/music_like/宗次郎-故乡的原风景.mp3'
     }
 ]
