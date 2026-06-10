393b848d29a91221f72cf5d6511f67198b3f7a6b
leeao <34832462@qq.com>
2026-06-10 09:20:56 +0800

    T

--- js/music_like.js
+++ js/music_like.js
@@ -2,32 +2,32 @@ var music = [
     {
         name: 'Somebody That I Used To Know',
         author: 'Gotye&Kimbra',
-        url: 'https://life.leeao8.cn/music_like/Somebody That I Used To Know.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Somebody.mp3'
     },
     {
         name: 'Snowdreams',
         author: '班得瑞',
-        url: 'https://life.leeao8.cn/music_like/Bandari-Snowdreams.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Bandari-Snowdreams.mp3'
     },
     {
         name: '安妮的仙境',
         author: '班得瑞',
-        url: 'https://life.leeao8.cn/music_like/Bandari-安妮的仙境.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Bandari-AnnieWonderland.mp3'
     }, {
         name: '初雪',
         author: '班得瑞',
-        url: 'https://life.leeao8.cn/music_like/Bandari-初雪.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Bandari-FirstSnowFlakes.mp3'
     }, {
         name: '卡布里的月光',
         author: '班得瑞',
-        url: 'https://life.leeao8.cn/music_like/Bandari-卡布里的月光.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Bandari-Moonlight.mp3'
     }, {
         name: '童年记忆',
         author: '班得瑞',
-        url: 'https://life.leeao8.cn/music_like/Bandari-童年记忆.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/Bandari-Childhood.mp3'
     }, {
         name: '故乡的原风景',
         author: '宗次郎',
-        url: 'https://life.leeao8.cn/music_like/宗次郎-故乡的原风景.mp3'
+        url: 'https://pmfxqcqvqrhxnxyotyzp.supabase.co/storage/v1/object/public/my/music_like/SceneryOfHometown.mp3'
     }
 ]
